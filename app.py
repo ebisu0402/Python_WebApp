36a442cb0275886c20fd930d894ff0644275b3c5
ebisu0402 <oca220424003@edu.oca.ac.jp>
2024-06-22 17:41:59 +0900

    最後がよくわからなかった

--- app.py
+++ app.py
@@ -1,51 +1,34 @@
-from flask import Flask
-from flask import Flask, request
-from flask import Flask, request,render_template
-from flask import Flask, request, jsonify
-import requests
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from test_model import Person  # test_model.py内でPersonモデルが定義されていると仮定
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test_db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 
-@app.route("/")
-def inex():
-    return "Response Data"
+# ファイルのダウンロードと配置後に以下の関数を追加する
+@app.route("/person_search")
+def person_search():
+    return render_template("person_search.html")
 
 
-@app.route("/another")
-def another():
-    return "Another Response"
+@app.route("/person_result")
+def person_result():
+    search_size = request.args.get("search_size")
+    persons = db.session.query(Person).filter(Person.size > search_size)
+    return render_template(
+        "./person_result.html", persons=persons, search_size=search_size
+    )
 
 
-@app.route('/test_request')
-def test_request():
-    return f'test_request:{request.args.get("dummy")}'
+# SQLAlchemyのモデルを定義（humanテーブルの定義と同じものを想定）
+class Human(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    height = db.Column(db.Float)
+    weight = db.Column(db.Float)
 
-
-@app.route('/exercise_request/<data>')
-def test_request_param(data):
-    return f'exercise_request:{data}'
-
-@app.route('/show_html')
-def show_html():
-    return render_template('test_html.html')
-
-@app.route('/exercise', methods=['GET', 'POST'])
-def exercise():
-    if request.method == 'POST':
-        my_name = request.form.get('my_name', '') 
-        return f'Hello, {my_name}!'  
-    return render_template('exercise.html')
-
-@app.route('/try_rest', methods=['POST'])
-def try_rest():
-    # リクエストデータをJSONとして受け取る
-    request_json = request.get_json()
-    print(request_json)
-    print(type(request_json))
-    name = request_json['name']
-    print(name)
-    response_json = {"response_json": request_json}
-    return jsonify(response_json)
-
-
+    def __repr__(self):
+        return f"<Human {self.id}: {self.name}, {self.height}cm, {self.weight}kg>"
